f3ecb8b9e3edb0b7bd294fcfcecd66069e162360
dontech09 <donnjondanmakal@gmail.com>
2022-04-22 21:22:13 +0530

    task and User get

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 
 const User = require("./models/user");
+const Task = require("./models/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -19,6 +20,65 @@ app.post("/users", (req, res) => {
       res.send(e);
     });
   console.log(req.body);
+});
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+app.post("/task", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400);
+      res.send(e);
+    });
+  console.log(req.body);
+});
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+//Mongoosse for other methods
+
+app.get("/task", (req, res) => {
+  Task.find({})
+    .then((task) => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/task/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send("Not Found");
+      }
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
 });
 app.listen(port, () => {
   console.log(port);
